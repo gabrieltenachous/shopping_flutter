f0059fece98680c1e17f863ea74a9e74cd67c76f
gabrieltenachous <58226659+gabrieltenachous@users.noreply.github.com>
2021-05-06 16:57:00 -0300

    Layout Finalizado :D

---
+++ lib/pages/Product.page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class ProductPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              backgroundColor: Colors.white.withOpacity(0),
+              elevation: 0.0,
+              expandedHeight: 500,
+              floating: false,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                centerTitle: true,
+                background: Image.asset(
+                  "assets/product-10.png",
+                  width: double.infinity,
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+            ),
+          ];
+        },
+        body: ListView(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(
+                top: 10,
+                left: 10,
+                right: 10,
+              ),
+              child: Text(
+                "Dry Fit Long Sleeve",
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Text(
+                "Details",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Text(
+                "Nike Dri is a polyester fabric designed to helpyou keep dry so you can more comfortably work, longer",
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -1,11 +1,11 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shopping/pages/Product.page.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
+      body: Padding(
         padding: EdgeInsets.all(15),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -14,9 +14,165 @@ class HomePage extends StatelessWidget {
               height: 60,
             ),
             search(),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              "Categories",
+              style: TextStyle(fontSize: 30),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 90,
+              child: categoryList(),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Best Salling",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                FlatButton(
+                  onPressed: () => {},
+                  child: Text("See All"),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 350,
+              child: productList(
+                context,
+              ),
+            ),
           ],
         ),
       ),
+    );
+  }
+
+  Widget productItem(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      margin: EdgeInsets.all(5),
+      width: 170,
+      color: Colors.black12,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ProductPage(),
+                ),
+              );
+            },
+            child: Image.asset(
+              "assets/product-1.png",
+              width: 170,
+              height: 170,
+              fit: BoxFit.cover,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            height: 60,
+            child: Text(
+              "Titule do produto",
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            "Marca",
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w300,
+            ),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            "\$ 200",
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Color(0xFF00C569),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget productList(BuildContext context) {
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      children: [
+        productItem(context),
+        productItem(context),
+        productItem(context),
+      ],
+    );
+  }
+
+  Widget categoryList() {
+    return Container(
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: [
+          categoryItem(),
+          categoryItem(),
+          categoryItem(),
+          categoryItem(),
+          categoryItem(),
+        ],
+      ),
+    );
+  }
+
+  Widget categoryItem() {
+    return Container(
+      width: 70,
+      height: 70,
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          new BoxShadow(
+            color: Colors.black12,
+            offset: new Offset(1, 1),
+            blurRadius: 5,
+            spreadRadius: 2,
+          ),
+        ],
+        borderRadius: BorderRadius.all(
+          Radius.circular(64),
+        ),
+      ),
+      child: Image.asset("assets/Icon_Devices.png"),
     );
   }
 
